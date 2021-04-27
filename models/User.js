484f111c4067ccd1672b0709d9f935695dd3b980
SoonKi Kwon <jhk327@gmail.com>
2021-04-27 11:52:03 +0900

    [#1][Add] create models

--- models/User.js
+++ models/User.js
@@ -1,2 +1,42 @@
 'use strict';
 const mongoose = require('mongoose');
+
+//TODO:minlength, maxlength 같은 옵션은 나중에. 테스트 번거로우니까!
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: 1,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    bookmark: [
+      {
+        _id: false,
+        contentId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Content',
+          required: true,
+        },
+      },
+    ],
+    catInfo: {
+      name: { type: String, required: true, trim: true },
+      age: { type: String, required: true, trim: true },
+      gender: { type: String, required: true, trim: true },
+      image: { type: String },
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model('User', userSchema);
