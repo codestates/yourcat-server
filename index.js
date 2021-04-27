fe022e2b1c107fea355d9107aac2e096d84fcdda
SoonKi Kwon <jhk327@gmail.com>
2021-04-27 10:52:22 +0900

    [#1][Add] connect local server

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-//
+'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+// const usersModel = require('./models/User');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log('mongoDB연결 에러 : ', err));
+
+app.get('/', (req, res) => {
+  res.json({ message: '연결되었습니다.' });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
