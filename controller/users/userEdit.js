7e24894f40bbb69cb971175a0406d40c549e0566
SoonKi Kwon <jhk327@gmail.com>
2021-04-29 11:00:49 +0900

    [#9][Fix] userEdit API

--- controller/users/userEdit.js
+++ controller/users/userEdit.js
@@ -1,15 +1,23 @@
 const User = require('../../models/User');
 
 // body에는 nickname, password, catInfo가 올 수 있다.
+// 수정페이지에서 이값들이 비어있으면 어떻게할까~
+// 일단 수정페이지의 패스워드빼고는 다 채워진채로 렌더되는 방식으로 고고
+// 패스워드가 비어있으면 패스워드는 빼고 업데이트
 module.exports = async (req, res) => {
   try {
     const { _id } = req.user;
-    const { nickname } = req.body;
+    const { nickname, password, catInfo } = req.body;
+    if (password.length !== 0) {
+      req.data = { nickname, password, catInfo };
+    } else {
+      req.data = { nickname, catInfo };
+    }
     const existUser = await User.findOne({ nickname });
     if (existUser) {
       res.status(401).json({ message: '이미 존재하는 닉네임입니다.' });
     } else {
-      User.updateOne({ _id }, { $set: req.body }, (err, data) => {
+      User.updateOne({ _id }, { $set: req.data }, (err, data) => {
         if (err) {
           res.status(400).json({ message: '사용자를 찾을 수 없습니다.' });
         } else if (data) {
